edccbb6f25e2959761ae0cf2b3aba04c31efd239
RCS <67485297+ravit-cohen-segev@users.noreply.github.com>
2021-07-19 23:57:35 +0300

    Add files via upload

--- code/landmarks_detection/create_dataset_functions.py
+++ code/landmarks_detection/create_dataset_functions.py
@@ -10,13 +10,13 @@ import os
 import cv2
 
 class create_dataset_dict():
-    def __init__(self,path):
+    def __init__(self,path, i):
         self.path = path
         self.annotated = {}
-        self.i = 0
+        self.i = i
         
     def cats_annotated_in_dir(self, d):
-        full_path = os.join(self.path,d)
+        full_path = os.path.join(self.path,d)
         files = os.listdir(full_path)
         for file in files:
             #check if file is with '.txt' extension. if not than continue to the next file
@@ -27,9 +27,9 @@ class create_dataset_dict():
             df_annot = pd.read_csv(anot_path, header=None, sep='\t').T
             annot_arr = df_annot.to_numpy()
             #read image
-            image_file = file.replace('.txt','.png')
-            image_path = os.path.join(self.path, image_file)
-            image_arr = cv2.imread(image_path)
+            image_file = file.replace('.txt','.tif')
+            image_path = os.path.join(full_path, image_file)
+            image_arr = cv2.imread(image_path, 1)
             self.annotated[str(self.i)] = [image_arr, annot_arr]
             #move to the next index/key
             self.i += 1
@@ -44,10 +44,19 @@ class create_dataset_dict():
         return self.annotated
             
           
- if __name__=="__main__":
+if __name__=="__main__":
     dir_path_clinical = r"C:\Users\ravit\PycharmProject\cats\Laurens_dataset\Videos_From_Lauren\Cat_pain_data_for_AI_collaboration\pain_no_pain_data_clinical_population\video_data\Annotated_images_sorted_by_condition"
-    dir_path_div = r"C:/Users/ravit/PycharmProject/cats/Laurens_dataset/Videos_From_Lauren/Cat_pain_data_for_AI_collaboration/pain_no_pain_data_diverse_population_"
+    dir_path_div_pain = r"C:/Users/ravit/PycharmProject/cats/Laurens_dataset/Videos_From_Lauren/Cat_pain_data_for_AI_collaboration/pain_no_pain_data_diverse_population_/pain"
+    dir_path_div_no_pain = r"C:/Users/ravit/PycharmProject/cats/Laurens_dataset/Videos_From_Lauren/Cat_pain_data_for_AI_collaboration/pain_no_pain_data_diverse_population_/no_pain"
     
-    create_data = create_dataset_dict(dir_path_clinical)
+    create_data = create_dataset_dict(dir_path_clinical, 0)
     dataset_dict = create_data.create_dict()
-    
+
+    create_data_div_pain = create_dataset_dict(dir_path_div_pain, len(dataset_dict))
+    dataset_div_pain = create_data_div_pain.create_dict()
+
+    create_data_div_no_pain = create_dataset_dict(dir_path_div_no_pain, len(dataset_dict)+len(dataset_div_pain))
+    dataset_div_no_pain = create_data_div_no_pain.create_dict()
+
+    dataset_dict.update(dataset_div_pain)
+    dataset_dict.update(dataset_div_no_pain)
